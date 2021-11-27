40c0b9c494a1d590aa43d5a2362890f9e12c4e29
David Jagoe <david@jagoe.eu>
2021-11-26 17:05:45 -0800

    Better linked list.

--- Chapter1/llist.c
+++ Chapter1/llist.c
@@ -9,75 +9,100 @@
 // int MAXLEN = pow(2, sizeof(uint) * 8) - 1; // How to do this at comptime?
 
 
-struct node_t {
+struct node {
   int content;
-  struct node_t *next;
-  /* struct node_t *prev; */
+  struct node *next;
 };
 
-typedef struct node_t node;
-typedef struct node_t list;
+typedef struct node node_t;
+typedef node_t **list_t;
+/* typedef node_t **list_t; */
 
 
-node *node_create(int content) {
-  node *n = (node *)malloc(sizeof(node));
-  n->content = content;
-  n->next = NULL;
-  return n;
-}
+/* node_t *node_create(int content) { */
+/*   node_t *n = (node_t *)malloc(sizeof(node_t)); */
+/*   n->content = content; */
+/*   n->next = NULL; */
+/*   return n; */
+/* } */
 
-node *list_create() {
-  return node_create(0);
-}
-
-
-void list_destroy(node *list) {
-  // HOLD for freeing nodes.
-  free(list);
+list_t list_create() {
+  list_t list = malloc(sizeof(node_t *));
+  return list;
 }
 
 
-node *list_insert(node *list, uint index, int value) {
-  node *before = NULL;
-  node *new = node_create(value);
-  node *after = list;
-  /* for (int i = 0; i < index; i++) { */
+/* void list_destroy(node_t **list) { */
+/*   // HOLD for freeing nodes. */
+/*   /\* free(list); *\/ */
+/* } */
+
+
+/* node_t *list_insert(node_t *list, uint index, int value) { */
+/*   node_t *before = NULL; */
+/*   node_t *new = node_create(value); */
+/*   node_t *after = list; */
+/*   /\* for (int i = 0; i < index; i++) { *\/ */
     
-  /* } */
-  if (before != NULL) {
-    before->next = new;
-  }
-  new->next = after;
-  return new;
+/*   /\* } *\/ */
+/*   if (before != NULL) { */
+/*     before->next = new; */
+/*   } */
+/*   new->next = after; */
+/*   return new; */
+/* } */
+
+
+/* uint list_length(node_t *list) { */
+/*   uint length = 0; */
+/*   node_t *iter = list; */
+/*   while (iter->next) { */
+/*     iter = iter->next; */
+/*     length++; */
+/*   } */
+/*   return length; */
+/* } */
+
+
+node_t *node_create(int value) {
+  node_t *node = (node_t *)malloc(sizeof(node_t));
+  node->content = value;
+  return node;
 }
 
 
-uint list_length(node *list) {
-  uint length = 0;
-  node* iter = list;
-  while (iter->next) {
-    iter = iter->next;
-    length++;
+int list_insert(list_t list, uint position, int value) {
+  node_t *node = node_create(value);
+  if (*list == NULL) {
+    list = &node;
+    return 0;
   }
-  return length;
+  return -1;
+}
+
+
+bool is_empty(list_t list) {
+  return *list == NULL;
 }
 
 
 void test_create_empty_list() {
-  list *empty = list_create();
-  assert(list_length(empty) == 0);
+  list_t empty = list_create();
+  assert(is_empty(empty));
+  /* assert(list_length(empty) == 0); */
 }
 
 
-void test_insert_element() {
-  list *l = list_create();
-  l = list_insert(l, 0, 1);
-  assert(list_length(l) == 1);
+void test_insert_first_element() {
+  list_t list = list_create();
+  list_insert(list, 0, 1);
+   /* assert((*list)->content == 1); */
+  /* assert(list_length(l) == 1); */
 }
 
 
 int main(int argc, char **argv) {
   test_create_empty_list();
-  test_insert_element();
+  test_insert_first_element();
   return 0;
 }
