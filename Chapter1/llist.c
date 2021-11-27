244c07d0bea970e5c7a19b61056fded0736706f8
David Jagoe <david@jagoe.eu>
2021-11-26 20:55:04 -0800

    Working linked list (WIP).

--- Chapter1/llist.c
+++ Chapter1/llist.c
@@ -16,15 +16,7 @@ struct node {
 
 typedef struct node node_t;
 typedef node_t **list_t;
-/* typedef node_t **list_t; */
 
-
-/* node_t *node_create(int content) { */
-/*   node_t *n = (node_t *)malloc(sizeof(node_t)); */
-/*   n->content = content; */
-/*   n->next = NULL; */
-/*   return n; */
-/* } */
 
 list_t list_create() {
   list_t list = malloc(sizeof(node_t *));
@@ -38,32 +30,6 @@ list_t list_create() {
 /* } */
 
 
-/* node_t *list_insert(node_t *list, uint index, int value) { */
-/*   node_t *before = NULL; */
-/*   node_t *new = node_create(value); */
-/*   node_t *after = list; */
-/*   /\* for (int i = 0; i < index; i++) { *\/ */
-    
-/*   /\* } *\/ */
-/*   if (before != NULL) { */
-/*     before->next = new; */
-/*   } */
-/*   new->next = after; */
-/*   return new; */
-/* } */
-
-
-/* uint list_length(node_t *list) { */
-/*   uint length = 0; */
-/*   node_t *iter = list; */
-/*   while (iter->next) { */
-/*     iter = iter->next; */
-/*     length++; */
-/*   } */
-/*   return length; */
-/* } */
-
-
 node_t *node_create(int value) {
   node_t *node = (node_t *)malloc(sizeof(node_t));
   node->content = value;
@@ -72,12 +38,36 @@ node_t *node_create(int value) {
 
 
 int list_insert(list_t list, uint position, int value) {
-  node_t *node = node_create(value);
-  if (*list == NULL) {
-    list = &node;
-    return 0;
+  node_t *new = node_create(value);
+  node_t *before = NULL;
+  node_t *after = *list;
+
+  for (int i = 0; i < position; i++) {
+    before = after;
+    after = after->next;
   }
+
+  new->next = after;
+  if (before) {
+    before->next = new;
+  }
+
+  if (position == 0) {
+    *list = new;
+  }
+  
   return -1;
+}
+
+
+uint list_length(list_t list) {
+  uint length = 0;
+  node_t *head = *list;
+  while (head) {
+    head = head->next;
+    length++;
+  }
+  return length;
 }
 
 
@@ -89,20 +79,33 @@ bool is_empty(list_t list) {
 void test_create_empty_list() {
   list_t empty = list_create();
   assert(is_empty(empty));
-  /* assert(list_length(empty) == 0); */
+  assert(list_length(empty) == 0);
 }
 
 
 void test_insert_first_element() {
   list_t list = list_create();
   list_insert(list, 0, 1);
-   /* assert((*list)->content == 1); */
-  /* assert(list_length(l) == 1); */
+  assert((*list)->content == 1);
+  assert(list_length(list) == 1);
+}
+
+
+void test_insert_multiple_elements_front() {
+  list_t list = list_create();
+  list_insert(list, 0, 1);
+  list_insert(list, 0, 2);
+  list_insert(list, 0, 3);
+  assert((*list)->content == 3);
+  assert((*list)->next->content == 2);
+  assert((*list)->next->next->content == 1);
+  assert(list_length(list) == 3);
 }
 
 
 int main(int argc, char **argv) {
   test_create_empty_list();
   test_insert_first_element();
+  test_insert_multiple_elements_front();
   return 0;
 }
